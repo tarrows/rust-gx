aeba9cb2b454ef72cb420c4eafc44d3aa824e201
tarrows <rem_tate@yahoo.co.jp>
2020-10-25 01:16:34 +0900

    move updates and generates to outside of polling events

--- src/game.rs
+++ src/game.rs
@@ -5,9 +5,10 @@ use sdl2::rect::Rect;
 use std::time::Duration;
 
 pub struct Game {
-  context: sdl2::Sdl,
+  // context: sdl2::Sdl,
   // window: sdl2::video::Window,
   timer: sdl2::TimerSubsystem, // TODO: Consider to use time crate or else as recommended in TimerSubsystem
+  events: sdl2::EventPump,
   canvas: sdl2::render::Canvas<sdl2::video::Window>,
   ticks_count: u32,
   is_running: bool,
@@ -46,6 +47,7 @@ impl Game {
   pub fn init(config: Config) -> Result<Game, String> {
     let context = sdl2::init()?;
     let timer = context.timer()?;
+    let events = context.event_pump()?;
     let video = context.video()?;
     let window = video
       .window(TITLE, config.width, config.height)
@@ -63,13 +65,18 @@ impl Game {
       x: config.width as f64 / 2.0,
       y: config.height as f64 / 2.0,
     };
+    let vel_ball = Vector2 {
+      x: -200.0,
+      y: 235.0,
+    };
 
     println!("complete initialize...");
 
     let game = Game {
-      context: context,
+      // context: context,
       // window: window,
       timer: timer,
+      events: events,
       canvas: canvas,
       ticks_count: 0,
       is_running: true,
@@ -79,21 +86,22 @@ impl Game {
       dir_paddle: PaddleDirection::Stop,
       pos_paddle: pos_paddle,
       pos_ball: pos_ball,
-      vel_ball: Vector2 {
-        x: -200.0,
-        y: 235.0,
-      },
+      vel_ball: vel_ball,
     };
 
     Ok(game)
   }
 
   pub fn run_loop(&mut self) {
+    //    let mut event_pump = self.context.event_pump().unwrap();
+
     while self.is_running {
+      self.dir_paddle = PaddleDirection::Stop;
+      // for event in event_pump.poll_iter() {
       self.process_input();
+      // }
       self.update();
       self.generate_output();
-
       // 60fps
       ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
@@ -104,42 +112,42 @@ impl Game {
   }
 
   fn process_input(&mut self) {
-    let mut event_pump = self.context.event_pump().unwrap();
+    // let mut event_pump = self.context.event_pump().unwrap();
 
     // TODO: Move loop to "run_loop"
-    'running: loop {
-      for event in event_pump.poll_iter() {
-        self.dir_paddle = PaddleDirection::Stop;
+    // 'running: loop {
+    for event in self.events.poll_iter() {
+      // self.dir_paddle = PaddleDirection::Stop;
 
-        match event {
-          Event::Quit { .. }
-          | Event::KeyDown {
-            keycode: Some(Keycode::Escape),
-            ..
-          } => {
-            self.is_running = false;
-            break 'running;
-          }
-          Event::KeyDown {
-            keycode: Some(Keycode::W),
-            ..
-          } => {
-            // println!("Up");
-            self.dir_paddle = PaddleDirection::Up;
-            break 'running;
-          }
-          Event::KeyDown {
-            keycode: Some(Keycode::S),
-            ..
-          } => {
-            // println!("Down");
-            self.dir_paddle = PaddleDirection::Down;
-            break 'running;
-          }
-          _ => break 'running,
+      match event {
+        Event::Quit { .. }
+        | Event::KeyDown {
+          keycode: Some(Keycode::Escape),
+          ..
+        } => {
+          self.is_running = false;
+          // break 'running;
         }
+        Event::KeyDown {
+          keycode: Some(Keycode::W),
+          ..
+        } => {
+          // println!("Up");
+          self.dir_paddle = PaddleDirection::Up;
+          // break 'running;
+        }
+        Event::KeyDown {
+          keycode: Some(Keycode::S),
+          ..
+        } => {
+          // println!("Down");
+          self.dir_paddle = PaddleDirection::Down;
+          // break 'running;
+        }
+        _ => {} // break 'running,
       }
     }
+    // }
   }
 
   fn update(&mut self) {
@@ -147,7 +155,7 @@ impl Game {
     // substitute https://github.com/emscripten-ports/SDL2/blob/master/include/SDL_timer.h
     while self.ticks_count + 16 > self.timer.ticks() {
       // println!("{} < {}", self.ticks_count, self.timer.ticks());
-      ::std::thread::sleep(Duration::new(0, 1000000)); // wait 1ms
+      ::std::thread::sleep(Duration::new(0, 1_000_000u32)); // wait 1ms
     }
     // self.ticks_count + 16 - self.timer.ticks() <= 0 -> panicked at 'attempt to subtract with overflow'
     // self.ticks_count.saturating_add(16).saturating_sub(self.timer.ticks()) <= 0 -> never breaks
@@ -192,11 +200,13 @@ impl Game {
     let paddle_x = (self.pos_paddle.x - THICKNESS as f64 / 2.0).trunc() as i32;
     let paddle_y = (self.pos_paddle.y - PADDLE_HEIGHT / 2.0).trunc() as i32;
     let paddle = Rect::new(paddle_x, paddle_y, THICKNESS, PADDLE_HEIGHT as u32);
+    // self.paddle.reposition((paddle_x, paddle_y));
     self.canvas.fill_rect(paddle).unwrap(); // TODO: switch to Result<T, E>
 
     let ball_x = (self.pos_ball.x - THICKNESS as f64 / 2.0).trunc() as i32;
     let ball_y = (self.pos_ball.y - THICKNESS as f64 / 2.0).trunc() as i32;
     let ball = Rect::new(ball_x, ball_y, THICKNESS, THICKNESS);
+    // self.ball.reposition((ball_x, ball_y));
     self.canvas.fill_rect(ball).unwrap(); // TODO: switch to Result<T, E>
 
     self.canvas.present();
