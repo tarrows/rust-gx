600b67697d480ddb81d8094f190333f968e450de
tarrows <rem_tate@yahoo.co.jp>
2020-10-26 01:58:52 +0900

    Clean-up code

--- src/game.rs
+++ src/game.rs
@@ -7,11 +7,10 @@ use std::time::Duration;
 
 pub struct Game {
   // SDL2 context items
-  // context: sdl2::Sdl,
-  // window: sdl2::video::Window,
   timer: sdl2::TimerSubsystem, // TODO: Consider to use time crate or else as recommended in TimerSubsystem
   events: sdl2::EventPump,
   canvas: sdl2::render::Canvas<sdl2::video::Window>,
+
   // Global state
   ticks_count: u32,
   is_running: bool,
@@ -46,6 +45,7 @@ struct Vector2 {
 const TITLE: &str = "Pong";
 const THICKNESS: u32 = 15;
 const PADDLE_HEIGHT: f64 = 100.0;
+const PADDLE_SPEED: f64 = 300.0;
 
 impl Game {
   pub fn init(config: Config) -> Result<Game, String> {
@@ -77,8 +77,6 @@ impl Game {
     println!("complete initialize...");
 
     let game = Game {
-      // context: context,
-      // window: window,
       timer: timer,
       events: events,
       canvas: canvas,
@@ -97,13 +95,9 @@ impl Game {
   }
 
   pub fn run_loop(&mut self) {
-    //    let mut event_pump = self.context.event_pump().unwrap();
-
     while self.is_running {
       self.dir_paddle = PaddleDirection::Stop;
-      // for event in event_pump.poll_iter() {
       self.process_input();
-      // }
       self.update();
       self.generate_output();
       // 60fps
@@ -116,13 +110,7 @@ impl Game {
   }
 
   fn process_input(&mut self) {
-    // let mut event_pump = self.context.event_pump().unwrap();
-
-    // TODO: Move loop to "run_loop"
-    // 'running: loop {
     for event in self.events.poll_iter() {
-      // self.dir_paddle = PaddleDirection::Stop;
-
       match event {
         Event::Quit { .. }
         | Event::KeyDown {
@@ -130,25 +118,20 @@ impl Game {
           ..
         } => {
           self.is_running = false;
-          // break 'running;
         }
         Event::KeyDown {
           keycode: Some(Keycode::W),
           ..
         } => {
-          // println!("Up");
           self.dir_paddle = PaddleDirection::Up;
-          // break 'running;
         }
         Event::KeyDown {
           keycode: Some(Keycode::S),
           ..
         } => {
-          // println!("Down");
           self.dir_paddle = PaddleDirection::Down;
-          // break 'running;
         }
-        _ => {} // break 'running,
+        _ => {}
       }
     }
     // }
@@ -158,7 +141,6 @@ impl Game {
     // SDL_TICKS_PASSED seems not exists on rust-sdl2
     // substitute https://github.com/emscripten-ports/SDL2/blob/master/include/SDL_timer.h
     while self.ticks_count + 16 > self.timer.ticks() {
-      // println!("{} < {}", self.ticks_count, self.timer.ticks());
       ::std::thread::sleep(Duration::new(0, 1_000_000u32)); // wait 1ms
     }
     // self.ticks_count + 16 - self.timer.ticks() <= 0 -> panicked at 'attempt to subtract with overflow'
@@ -167,22 +149,17 @@ impl Game {
     let current = self.timer.ticks();
     let delta_time = (current - self.ticks_count) as f64 / 1000.0;
     let delta_time = f64::min(delta_time, 0.05);
-    // println!("delta: {}", delta_time);
     self.ticks_count = current;
 
     // TODO: Remove this if statement (maybe unnecessary)
     if self.dir_paddle != PaddleDirection::Stop {
-      let paddle_speed = 300.0;
-      let paddle_delta = (self.dir_paddle as i32 as f64) * paddle_speed * delta_time;
+      let paddle_delta = (self.dir_paddle as i32 as f64) * PADDLE_SPEED * delta_time;
       self.pos_paddle.y += paddle_delta;
-      // println!("paddleY: {} ({})", self.pos_paddle.y, paddle_delta);
 
       // Make sure paddle does not move off screen!
       // TODO: move these bounds to game object's field
       let lower_bound = PADDLE_HEIGHT / 2.0 + THICKNESS as f64;
       let upper_bound = self.window_height as f64 - PADDLE_HEIGHT / 2.0 - THICKNESS as f64;
-
-      // println!("(bounded)paddleY: {} < {} < {}", lower_bound, self.pos_paddle.y, upper_bound);
 
       // ref. https://doc.rust-lang.org/std/primitive.f64.html#method.clamp
       self.pos_paddle.y = self.pos_paddle.y.min(upper_bound).max(lower_bound);
@@ -208,7 +185,7 @@ impl Game {
     }
 
     // Did the ball collide with the right wall?
-    if self.pos_ball.x >= 1024.0 - THICKNESS as f64 && self.vel_ball.x > 0.0 {
+    if self.pos_ball.x >= (self.window_width - THICKNESS) as f64 && self.vel_ball.x > 0.0 {
       self.vel_ball.x *= -1.0
     }
 
@@ -218,7 +195,7 @@ impl Game {
     }
 
     // Did the ball collide with the bottom wall?
-    if self.pos_ball.y >= (768 - THICKNESS).into() && self.vel_ball.y > 0.0 {
+    if self.pos_ball.y >= (self.window_height - THICKNESS) as f64 && self.vel_ball.y > 0.0 {
       self.vel_ball.y *= -1.0
     }
   }
@@ -245,13 +222,11 @@ impl Game {
     let paddle_x = (self.pos_paddle.x - THICKNESS as f64 / 2.0).trunc() as i32;
     let paddle_y = (self.pos_paddle.y - PADDLE_HEIGHT / 2.0).trunc() as i32;
     let paddle = Rect::new(paddle_x, paddle_y, THICKNESS, PADDLE_HEIGHT as u32);
-    // self.paddle.reposition((paddle_x, paddle_y));
     self.canvas.fill_rect(paddle).unwrap(); // TODO: switch to Result<T, E>
 
     let ball_x = (self.pos_ball.x - THICKNESS as f64 / 2.0).trunc() as i32;
     let ball_y = (self.pos_ball.y - THICKNESS as f64 / 2.0).trunc() as i32;
     let ball = Rect::new(ball_x, ball_y, THICKNESS, THICKNESS);
-    // self.ball.reposition((ball_x, ball_y));
     self.canvas.fill_rect(ball).unwrap(); // TODO: switch to Result<T, E>
 
     self.canvas.present();
